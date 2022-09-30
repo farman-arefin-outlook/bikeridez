4554f79a293d870cf2a89cecee0f6b0c52130b68
Tamim <farman.arefin1@outlook.com>
2022-09-30 16:25:50 +0600

    Add data and images

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyByfWMZqsLX0TQdUU1p0CgJPAJqa-Nj0pw",
+    authDomain: "bikeridez.firebaseapp.com",
+    projectId: "bikeridez",
+    storageBucket: "bikeridez.appspot.com",
+    messagingSenderId: "768757775185",
+    appId: "1:768757775185:web:0549620d2f7aaaf1dcf170"
+};
+
+export default firebaseConfig;
